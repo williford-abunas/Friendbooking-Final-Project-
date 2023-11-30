c133ee2242103f7104038741adf90a0dcdd077fc
MingGhee Fong <mingghee@hotmail.com>
2023-11-30 18:27:53 +1300

    Components placeholders established

--- client/components/User.tsx
+++ client/components/User.tsx
@@ -1,7 +1,48 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const DaysOfWeek = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+]
+
 export default function Users() {
+  const [selectedDay, setSelectedDay] = useState<string | null>(null)
+
+  const handleDayClick = (day: string) => {
+    setSelectedDay(selectedDay === day ? null : day)
+  }
+
+  function TimeSlotsDropdown() {
+    // Replace this with your actual time slots data
+    const timeSlots = ['10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM']
+
+    return (
+      <div className="time-slots-dropdown">
+        {timeSlots.map((slot, index) => (
+          // Use Link or button based on your navigation needs
+          <Link key={index} to={`/form/${selectedDay}/${slot}`}>
+            {slot}
+          </Link>
+        ))}
+      </div>
+    )
+  }
+
   return (
     <>
-      <h1>THIS IS USERS PAGE!</h1>
+      <h1>OWNER WEEKLY CALENDAR!</h1>
+      {DaysOfWeek.map((day, index) => (
+        <div key={index} className="day-container">
+          <button onClick={() => handleDayClick(day)}>{day}</button>
+          {selectedDay === day && <TimeSlotsDropdown />}
+        </div>
+      ))}
     </>
   )
 }
