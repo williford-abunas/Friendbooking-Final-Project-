14e640fc84f0a843aa61876edd80a8b8566c0136
James Crabtree <jamescrabtreenz17@gmail.com>
2023-12-04 15:21:14 +1300

    Add inital dark mode

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,8 +1,28 @@
-import { Link } from 'react-router-dom'
-import UserSignOut from './UserSignOut.tsx'
-import OwnerSignOut from './OwnerSignOut.tsx'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import UserSignOut from './UserSignOut.tsx';
+import OwnerSignOut from './OwnerSignOut.tsx';
 
 export default function Nav() {
+  const [isDarkMode, setIsDarkMode] = useState(
+    localStorage.getItem('darkMode') === 'true'
+  );
+
+  const toggleDarkMode = () => {
+    const newMode = !isDarkMode;
+    setIsDarkMode(newMode);
+    localStorage.setItem('darkMode', String(newMode));
+  };
+
+  useEffect(() => {
+    // Update theme on mount and when isDarkMode changes
+    document.body.classList.toggle('dark-mode', isDarkMode);
+
+    // Dynamically switch stylesheets
+    const link = document.getElementById('theme-stylesheet') as HTMLLinkElement;
+    link.href = isDarkMode ? 'dark.css' : 'styles.css';
+  }, [isDarkMode]);
+
   return (
     <>
       <nav>
@@ -11,7 +31,10 @@ export default function Nav() {
         </button>
         <UserSignOut />
         {/* <OwnerSignOut /> */}
+        <button onClick={toggleDarkMode}>
+          Toggle Dark Mode
+        </button>
       </nav>
     </>
-  )
+  );
 }
