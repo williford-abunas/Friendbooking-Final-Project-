2e7d70f8c5444880d7f24d9e0004ca8c8f3e915a
MingGhee Fong <mingghee@hotmail.com>
2023-12-04 15:54:46 +1300

    timeslot db table created

---
+++ models/Timeslot.ts
@@ -0,0 +1,5 @@
+export interface Timeslot {
+  date: string
+  startTime: string
+  endTime: string
+}
---
+++ server/db/migrations/20231204015626_timeslot.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('timeslot', function (table) {
+    table.increments('id').primary()
+    table.date('date')
+    table.time('start_time').notNullable()
+    table.time('end_time').notNullable()
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('timeslot')
+}
