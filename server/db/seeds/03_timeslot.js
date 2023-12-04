d6ed83a32f8960bfe68fa0624906fb093eafeed4
MingGhee Fong <mingghee@hotmail.com>
2023-12-05 09:42:42 +1300

    Added day coulmn in timeslot table

--- server/db/seeds/03_timeslot.js
+++ server/db/seeds/03_timeslot.js
@@ -6,8 +6,26 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('timeslot').del()
   await knex('timeslot').insert([
-    { id: 1, date: '2023-3-27', start_time: '09:00', end_time: '11:00' },
-    { id: 2, date: '2023-2-21', start_time: '09:00', end_time: '13:00' },
-    { id: 3, date: '2023-1-2', start_time: '06:00', end_time: '07:00' },
+    {
+      id: 1,
+      date: '2023-3-27',
+      day: 'Monday',
+      start_time: '09:00',
+      end_time: '11:00',
+    },
+    {
+      id: 2,
+      date: '2023-2-21',
+      day: 'Wednesday',
+      start_time: '09:00',
+      end_time: '13:00',
+    },
+    {
+      id: 3,
+      date: '2023-1-2',
+      day: 'Friday',
+      start_time: '06:00',
+      end_time: '07:00',
+    },
   ])
 }
