18c5deb12fe75b3a778aa9c2022a5a5e00090376
williford abunas <abunaswilliford.com>
2023-12-04 23:56:27 +1300

    clean-up User Component

--- client/components/User.tsx
+++ client/components/User.tsx
@@ -1,14 +1,11 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { Link } from 'react-router-dom'
-import { SetStateAction, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import WeekPicker from './WeekPicker'
 
 export default function Users() {
   const { isAuthenticated, loginWithRedirect, isLoading } = useAuth0()
   const { user } = useAuth0()
-
-  const [selectedWeekData, setSelectedWeekData] = useState(null)
-  const [selectedDay, setSelectedDay] = useState<string | null>(null)
 
   useEffect(() => {
     // Check authentication status and redirect if not authenticated
@@ -17,30 +14,8 @@ export default function Users() {
     }
   }, [isLoading, isAuthenticated, loginWithRedirect])
 
-  const handleWeekChange = (selectedWeek) => {
+  const handleWeekChange = (selectedWeek: object) => {
     console.log('Selected Week Changed:', selectedWeek)
-  }
-
-  useEffect(() => {
-    console.log(selectedWeekData)
-  }, [selectedWeekData])
-
-  const handleDayClick = (day: string) => {
-    setSelectedDay((prevDay) => (prevDay === day ? null : day))
-  }
-
-  function TimeSlotsDropdown() {
-    const timeSlots = ['10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM']
-
-    return (
-      <div>
-        {timeSlots.map((slot, index) => (
-          <Link key={index} to={`/form/${selectedDay}/${slot}`}>
-            <button id="timeSlotButton">{slot}</button>
-          </Link>
-        ))}
-      </div>
-    )
   }
 
   if (!isAuthenticated || isLoading) {
@@ -60,25 +35,6 @@ export default function Users() {
         )}
       </div>
       <WeekPicker onWeekChange={handleWeekChange} />
-      {selectedWeekData && (
-        <div id="dayContainer">
-          {selectedWeekData.daysofWeek.map((day) => (
-            <>
-              <div key={day.date.toISOString()}>
-                <button
-                  id="userViewDays"
-                  onClick={() => handleDayClick(day.day)}
-                >
-                  {day.day}
-                </button>
-              </div>
-              <div id="dropdownTimeSlot">
-                {selectedDay === day.day && <TimeSlotsDropdown />}
-              </div>
-            </>
-          ))}
-        </div>
-      )}
     </>
   )
 }
